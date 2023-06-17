55822323f1de6b527b43b7dff7b3b2f6f56a63ad
Hsing Hang Pao <kyjerry0424@gmail.com>
2023-06-17 11:29:08 -0400

    delete unused files and add banner and exhibit components

---
+++ sextant/src/Components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Banner.module.css'
+
+const Banner = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Sextant</h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/Components/Exhibit/Exhibit.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Exhibit.module.css'
+
+const Exhibit = () => {
+  return (
+    <div className={StyleSheet.container}>Exhibit</div>
+  )
+}
+
+export default Exhibit
